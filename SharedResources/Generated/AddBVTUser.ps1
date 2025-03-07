acd73d542b7a6fe67ba673472ada0677238e798d
tianyiliu <tianyiliu@microsoft.com>
2025-03-07 15:41:35 +0800

    add more domain users

--- SharedResources/Generated/AddBVTUser.ps1
+++ SharedResources/Generated/AddBVTUser.ps1
@@ -5,4 +5,23 @@ param
 )
 
 $secureDomainPassword = ConvertTo-SecureString $domainPassword -AsPlainText -Force
+
+New-ADUser -Name D_admin -AccountPassword $secureDomainPassword -Enabled $true
+New-ADUser -Name D_admin_E -AccountPassword $secureDomainPassword -Enabled $true
+New-ADUser -Name BVT_USER -AccountPassword $secureDomainPassword -Enabled $true
 New-ADUser -Name D_power -AccountPassword $secureDomainPassword -Enabled $true
+New-ADUser -Name D_power_E -AccountPassword $secureDomainPassword -Enabled $true
+New-ADUser -Name D_guest -AccountPassword $secureDomainPassword -Enabled $true
+New-ADUser -Name D_guest_E -AccountPassword $secureDomainPassword -Enabled $true
+
+New-ADUser -Name "Domain Admins" -AccountPassword $secureDomainPassword -Enabled $true
+New-ADUser -Name D_UIadmin -AccountPassword $secureDomainPassword -Enabled $true
+New-ADUser -Name D_JobAdmin -AccountPassword $secureDomainPassword -Enabled $true
+New-ADUser -Name D_JobAdmin1 -AccountPassword $secureDomainPassword -Enabled $true
+New-ADUser -Name D_JobOptr -AccountPassword $secureDomainPassword -Enabled $true
+New-ADUser -Name D_JobOptr1 -AccountPassword $secureDomainPassword -Enabled $true
+New-ADUser -Name D_User -AccountPassword $secureDomainPassword -Enabled $true
+New-ADUser -Name d_unicodepwd -AccountPassword $secureDomainPassword -Enabled $true
+New-ADUser -Name d_email1 -AccountPassword $secureDomainPassword -Enabled $true
+New-ADUser -Name d_email2 -AccountPassword $secureDomainPassword -Enabled $true
+New-ADUser -Name D_127cPwd -AccountPassword $secureDomainPassword -Enabled $true
