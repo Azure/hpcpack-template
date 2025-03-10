8568cad5dd97ea628429ee942a3c7ae73e15f1e0
tianyiliu <tianyiliu@microsoft.com>
2025-03-10 13:38:56 +0800

    use net user

--- SharedResources/Generated/AddBVTUser.ps1
+++ SharedResources/Generated/AddBVTUser.ps1
@@ -4,24 +4,22 @@ param
     [string] $domainPassword
 )
 
-$secureDomainPassword = ConvertTo-SecureString $domainPassword -AsPlainText -Force
+net user D_admin $domainPassword /add /domain
+net user D_admin_E $domainPassword /add /domain
+net user BVT_USER $domainPassword /add /domain
+net user D_power $domainPassword /add /domain
+net user D_power_E $domainPassword /add /domain
+net user D_guest $domainPassword /add /domain
+net user D_guest_E $domainPassword /add /domain
 
-New-ADUser -Name D_admin -AccountPassword $secureDomainPassword -Enabled $true
-New-ADUser -Name D_admin_E -AccountPassword $secureDomainPassword -Enabled $true
-New-ADUser -Name BVT_USER -AccountPassword $secureDomainPassword -Enabled $true
-New-ADUser -Name D_power -AccountPassword $secureDomainPassword -Enabled $true
-New-ADUser -Name D_power_E -AccountPassword $secureDomainPassword -Enabled $true
-New-ADUser -Name D_guest -AccountPassword $secureDomainPassword -Enabled $true
-New-ADUser -Name D_guest_E -AccountPassword $secureDomainPassword -Enabled $true
-
-New-ADUser -Name "Domain Admins" -AccountPassword $secureDomainPassword -Enabled $true
-New-ADUser -Name D_UIadmin -AccountPassword $secureDomainPassword -Enabled $true
-New-ADUser -Name D_JobAdmin -AccountPassword $secureDomainPassword -Enabled $true
-New-ADUser -Name D_JobAdmin1 -AccountPassword $secureDomainPassword -Enabled $true
-New-ADUser -Name D_JobOptr -AccountPassword $secureDomainPassword -Enabled $true
-New-ADUser -Name D_JobOptr1 -AccountPassword $secureDomainPassword -Enabled $true
-New-ADUser -Name D_User -AccountPassword $secureDomainPassword -Enabled $true
-New-ADUser -Name d_unicodepwd -AccountPassword $secureDomainPassword -Enabled $true
-New-ADUser -Name d_email1 -AccountPassword $secureDomainPassword -Enabled $true
-New-ADUser -Name d_email2 -AccountPassword $secureDomainPassword -Enabled $true
-New-ADUser -Name D_127cPwd -AccountPassword $secureDomainPassword -Enabled $true
+net user "Domain Admins" $domainPassword /add /domain
+net user D_UIadmin $domainPassword /add /domain
+net user D_JobAdmin $domainPassword /add /domain
+net user D_JobAdmin1 $domainPassword /add /domain
+net user D_JobOptr $domainPassword /add /domain
+net user D_JobOptr1 $domainPassword /add /domain
+net user D_User $domainPassword /add /domain
+net user D_unicodepwd $domainPassword /add /domain
+net user D_email1 $domainPassword /add /domain
+net user D_email2 $domainPassword /add /domain
+net user D_127cPwd $domainPassword /add /domain
